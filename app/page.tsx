1fa48f37faf043ded885b47e3721d080cb013adc
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-12-20 23:32:06 +0000

    adds outline and aria labels

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,12 @@ export default function Home() {
         </div>
         <div className="grid grid-rows-[1fr_min-content_1fr] gap-5 items-baseline">
           <span aria-hidden></span>
-          <span className="text-5xl">your_future_developer</span>
-          <nav className="text-xl font-semibold tracking-wider mx-auto flex gap-8 px-8 py-2 bg-gradient-to-r from-[#B3FFAB] to-[#12FFF7] skew-x-[22deg] [&>span]:skew-x-[-22deg]">
+          <span className="text-5xl" aria-label="your future developer">
+            your_future_developer
+          </span>
+          <nav className="text-xl font-semibold tracking-wider mx-auto flex gap-8 px-8 py-2 bg-gradient-to-r from-[#B3FFAB] to-[#12FFF7] skew-x-[22deg] [&>span]:skew-x-[-22deg] outline outline-2 outline-offset-2">
             <span>experience</span>
-            <span>about_me</span>
+            <span aria-label="about me">about_me</span>
             <span>contact</span>
           </nav>
         </div>
