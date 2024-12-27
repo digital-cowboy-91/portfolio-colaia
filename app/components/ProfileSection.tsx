2932b2e89590bb2595b6af0dcfe7fe3fd7ca5398
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-12-27 14:31:36 +0000

    responsive

--- app/components/ProfileSection.tsx
+++ app/components/ProfileSection.tsx
@@ -21,17 +21,19 @@ A self-taught JavaScript Software Developer with passion for innovation and expl
   `;
 
   return (
-    <section>
+    <section className="-mt-32 p-8">
       <div
-        className="bg-foreground w-[900px] h-[500px] rounded-[3rem] mx-auto p-8 flex gap-8 items-center"
+        className="bg-foreground rounded-[3rem] max-w-[900px] mx-auto p-8 flex gap-8 items-center max-md:flex-col-reverse"
         style={{
           filter: `drop-shadow(10px 10px 10px rgba(0, 0, 0, 0.5))`,
         }}
       >
         <Code snippet={snippet} />
-        <div className="rounded-full overflow-hidden">
-          <img src="https://placehold.co/500x500" />
-        </div>
+
+        <img
+          className="rounded-full max-w-[250px] max-h-[250px]"
+          src="https://placehold.co/250x250"
+        />
       </div>
     </section>
   );
