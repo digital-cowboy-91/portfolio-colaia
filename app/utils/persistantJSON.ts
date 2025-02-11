03050a45cabff7fd3a95428e9c5bcefb77716d3b
digital-cowboy-91 <dave.kolaja@gmail.com>
2025-02-11 12:38:19 +0000

    synchronize env var names

--- app/utils/persistantJSON.ts
+++ app/utils/persistantJSON.ts
@@ -15,7 +15,7 @@ export async function readFile<T extends DocumentName>(
   filename: T
 ): Promise<Documents[T]> {
   return fs
-    .readFile(process.env.DO_STORAGE + "/" + filename + ".json", {
+    .readFile(process.env.PERSISTANT + "/" + filename + ".json", {
       encoding: "utf8",
     })
     .then((res) => JSON.parse(res));
