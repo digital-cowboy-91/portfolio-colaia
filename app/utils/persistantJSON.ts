c2f7423de0833ebbbe6da275b54d192fa72a7373
digital-cowboy-91 <dave.kolaja@gmail.com>
2025-02-11 12:29:19 +0000

    rename storage env var

--- app/utils/persistantJSON.ts
+++ app/utils/persistantJSON.ts
@@ -15,7 +15,7 @@ export async function readFile<T extends DocumentName>(
   filename: T
 ): Promise<Documents[T]> {
   return fs
-    .readFile(process.env.SERVER_STORAGE + "/" + filename + ".json", {
+    .readFile(process.env.DO_STORAGE + "/" + filename + ".json", {
       encoding: "utf8",
     })
     .then((res) => JSON.parse(res));
