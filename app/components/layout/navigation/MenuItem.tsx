9615d6efd436c7f1fe22d2ac05da88fec4ccf42f
digital-cowboy-91 <dave.kolaja@gmail.com>
2025-03-03 00:25:48 +0000

    stop lenis to smooth scroll on link click

--- app/components/layout/navigation/MenuItem.tsx
+++ app/components/layout/navigation/MenuItem.tsx
@@ -1,3 +1,4 @@
+import { useLenis } from "lenis/react";
 import Link from "next/link";
 import { Bookmark } from ".";
 
@@ -7,9 +8,17 @@ interface Props {
 }
 
 export default function MenuItem({ bookmark: { id, title }, isActive }: Props) {
+  const lenis = useLenis();
+
   return (
     <Link
       href={`#${id}`}
+      onClick={(e) => {
+        e.preventDefault();
+        lenis?.scrollTo(`#${id}`, {
+          immediate: true,
+        });
+      }}
       className={`flex transition-all`}
       style={{
         color: isActive ? "var(--primary)" : undefined,
